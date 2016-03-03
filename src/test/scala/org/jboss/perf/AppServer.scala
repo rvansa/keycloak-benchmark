16d15152cd266f23b6502eeff39273d5c95b6beb
Radim Vansa <rvansa@redhat.com>
2016-03-03 14:35:20 +0100

    appserver stats

--- src/test/scala/org/jboss/perf/AppServer.scala
+++ src/test/scala/org/jboss/perf/AppServer.scala
@@ -2,6 +2,7 @@ package org.jboss.perf
 
 import java.net.InetSocketAddress
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.LongAdder
 import javax.ws.rs.core.Response
 import javax.ws.rs.{GET, POST, Path}
 
@@ -19,20 +20,30 @@ object AppServer {
   contextBuilder.getDeployment().getActualResourceClasses().add(classOf[AppServer])
   private val context = contextBuilder.bind(httpServer)
 
+  private val logouts = new LongAdder();
+  private val versions = new LongAdder();
+  private val pushNotBefores = new LongAdder();
+  private val queryBearerTokens = new LongAdder();
+  private val testAvailables = new LongAdder();
+
   def main(args: Array[String]): Unit = {
     httpServer.start()
     val timeout = Options.rampUp + Options.duration + Options.rampDown + 10;
     Thread.sleep(TimeUnit.SECONDS.toMillis(timeout))
     httpServer.stop(0);
+    printf("AppServer stats:%n%8d logout%n%8d version%n%8d pushNotBefore%n%8d queryBearerToken%n%8d testAvailables%n",
+      logouts.longValue(), versions.longValue(), pushNotBefores.longValue(), queryBearerTokens.longValue(), testAvailables.longValue())
   }
 }
 
 @Path("/admin")
 class AppServer {
+
   @GET
   @POST
   @Path(AdapterConstants.K_LOGOUT)
   def logout(): Response = {
+    AppServer.logouts.increment()
     Response.ok().build()
   }
 
@@ -40,6 +51,7 @@ class AppServer {
   @POST
   @Path(AdapterConstants.K_VERSION)
   def version(): Response = {
+    AppServer.versions.increment()
     Response.ok().build()
   }
 
@@ -47,6 +59,7 @@ class AppServer {
   @POST
   @Path(AdapterConstants.K_PUSH_NOT_BEFORE)
   def pushNotBefore(): Response = {
+    AppServer.pushNotBefores.increment()
     Response.ok().build()
   }
 
@@ -54,6 +67,7 @@ class AppServer {
   @POST
   @Path(AdapterConstants.K_QUERY_BEARER_TOKEN)
   def queryBearerToken(): Response = {
+    AppServer.queryBearerTokens.increment()
     Response.ok().build()
   }
 
@@ -61,6 +75,7 @@ class AppServer {
   @POST
   @Path(AdapterConstants.K_TEST_AVAILABLE)
   def testAvailable(): Response = {
+    AppServer.testAvailables.increment()
     Response.ok().build()
   }
 
